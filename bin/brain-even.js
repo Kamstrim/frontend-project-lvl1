b94a3c2dd0c7156595cdf22381c80f5931102fd4
Dmitriy Semenov <kamstrim@mail.ru>
2020-08-19 01:58:56 +0300

    step 5.1

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 import inputData from '../src/cli.js';
-import {checkNum} from '../src/even.js';
+import checkNum from '../src/even.js';
 
 console.log('Welcome to the Brain Games!');
 const name = inputData('May I have your name? ');
 console.log(`Hello, ${name}!`);
 console.log('Answer %c"yes" %cif the number is even, otherwise answer %c"no".', 'color: red;', 'font-weight:bold', 'color: red');
-var result = checkNum();
+const result = checkNum();
 if (result) {
-    console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${name}!`);
 } else {
-    console.log(`Let's try again, ${name}!`);
-};
+  console.log(`Let's try again, ${name}!`);
+}
--- src/even.js
+++ src/even.js
@@ -1,25 +1,25 @@
 import readlineSync from 'readline-sync';
 
-var getRandomIntInclusive = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+const getRandomIntInclusive = (min, max) => {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
 };
 
-export var checkNum = () => {
-    let i = 0;
-    while ( i !== 3) {
-        let number = getRandomIntInclusive(1, 30);
-        console.log(`Question: ${number}`);
-        var answer = readlineSync.question(`Your answer: `);
-        var correctAnswer = ( (number % 2) == 0) ? 'yes' : 'no';
-        if ( answer == correctAnswer) {
-            console.log(`Correct!`);
-            i += 1;
-        } else {
-            console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-            return false;
-        }
+export default () => {
+  let i = 0;
+  while (i !== 3) {
+    const number = getRandomIntInclusive(1, 30);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = ((number % 2) === 0) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      return false;
     }
-    return true;
+  }
+  return true;
 };
