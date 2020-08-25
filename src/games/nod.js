233b04d98bf7a4628c12307f3c4c4eb70335d811
Dmitriy Semenov <kamstrim@mail.ru>
2020-08-26 00:28:50 +0300

    step 7.1

--- src/games/nod.js
+++ src/games/nod.js
@@ -4,15 +4,15 @@ import { getRandomIntInclusive } from '../index.js';
 export default () => {
   let i = 0;
   while (i !== 3) {
-    let number1 = getRandomIntInclusive(1, 30);
-    let number2 = getRandomIntInclusive(1, 30);
+    const number1 = getRandomIntInclusive(1, 30);
+    const number2 = getRandomIntInclusive(1, 30);
     let correctAnswer = 1;
     console.log(`Question: ${number1} ${number2}`);
     const answer = readlineSync.question('Your answer: ');
     let nod = (number1 <= number2) ? number1 : number2;
-    while ((number1 % nod) || (number2 % nod) !== 0)  {
-        nod--;
-      }
+    while ((number1 % nod) || (number2 % nod) !== 0) {
+      nod -= 1;
+    }
     correctAnswer = nod;
     if (Number(answer) === correctAnswer) {
       console.log('Correct!');
