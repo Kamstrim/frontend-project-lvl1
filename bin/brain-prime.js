36f84ac5844aa3d4fcac973e4638048178a5042e
Dmitriy Semenov <kamstrim@mail.ru>
2020-08-26 01:10:42 +0300

    step 9

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import check from '../src/games/pr.js';
+import check from '../src/games/prime.js';
 import { inputName, finalText } from '../src/index.js';
 
 const name = inputName();
-console.log('What number is missing in the progression?');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 const result = check();
 finalText(result, name);
---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+import { getRandomIntInclusive } from '../index.js';
+
+export default () => {
+  let i = 0;
+  while (i !== 3) {
+    const number1 = getRandomIntInclusive(1, 100);
+    console.log(`Question: ${number1}`);
+    const answer = readlineSync.question('Your answer: ');
+    let n = 2;
+    while (number1 % n !== 0) {
+      n += 1;
+    }
+    const correctAnswer = (n === number1) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      return false;
+    }
+  }
+  return true;
+};
