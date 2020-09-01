74d6a2cab875c7e331eed771521d6969d393e266
Dmitriy Semenov <kamstrim@mail.ru>
2020-09-02 01:27:34 +0300

    update 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import inputData from '../src/cli.js';
+import { inputName } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const name = inputData('May I have your name? ');
-console.log(`Hello, ${name}!`);
+inputName();
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export default (quest) => readlineSync.question(quest);
+
+export const inputName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
