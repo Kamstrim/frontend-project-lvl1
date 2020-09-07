d2c6bb1101769683072ece73d15eec6b93565b3d
Dmitriy Semenov <kamstrim@mail.ru>
2020-09-07 22:43:43 +0300

    even update

--- src/games/even.js
+++ src/games/even.js
@@ -1,23 +1,5 @@
-import {
-  getRandomIntInclusive,
-  inputName,
-  inputAnswer,
-  checkAnswer,
-  finalText,
-}
-  from '../index.js';
+import { even } from '../index.js';
 
 export default () => {
-  const name = inputName();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let result = true;
-  const totalRound = 3;
-  for (let i = 0; (i < totalRound) && (result); i += 1) {
-    const number = getRandomIntInclusive(1, 30);
-    console.log(`Question: ${number}`);
-    const answer = inputAnswer();
-    const correctAnswer = ((number % 2) === 0) ? 'yes' : 'no';
-    result = (checkAnswer(answer, correctAnswer));
-  }
-  finalText(result, name);
+even();
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import inputData from './cli.js';
 
-export const inputName = () => {
+export const inputName = (text) => {
   console.log('Welcome to the Brain Games!');
   const name = inputData('May I have your name? ');
   console.log(`Hello, ${name}!`);
@@ -33,4 +33,19 @@ export const finalText = (result, name) => {
   } else {
     console.log(`Let's try again, ${name}!`);
   }
+};
+
+export const even = () => {
+  const name = inputName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let result = true;
+  const totalRound = 3;
+  for (let i = 0; (i < totalRound) && (result); i += 1) {
+    const number = getRandomIntInclusive(1, 30);
+    console.log(`Question: ${number}`);
+    const answer = inputAnswer();
+    const correctAnswer = ((number % 2) === 0) ? 'yes' : 'no';
+    result = (checkAnswer(answer, correctAnswer));
+  }
+  return finalText(result, name);
 };
