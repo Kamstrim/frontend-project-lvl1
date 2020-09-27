bd5f9169cf95fd30fae3c5b9a59e01164a4a1db1
Dmitriy Semenov <kamstrim@mail.ru>
2020-09-27 21:47:01 +0300

    new

--- src/games/even.js
+++ src/games/even.js
@@ -2,12 +2,12 @@ import { playGame, roundsCount } from '../index.js';
 import getRandomIntInclusive from '../rnd.js';
 
 export default () => {
-  const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
   const answersQuestions = []; // ['question','correctAnswer']
   for (let i = 0; i < roundsCount; i += 1) {
     const question = getRandomIntInclusive(1, 30);
     const correctAnswer = ((question % 2) === 0) ? 'yes' : 'no';
     answersQuestions[i] = [question, correctAnswer];
   }
-  playGame(gameQuestion, answersQuestions);
+  playGame(gameDescription, answersQuestions);
 };
