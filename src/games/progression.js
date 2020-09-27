3225d4d40300362f0e9b0b289eba2f31ccd1edfa
Dmitriy Semenov <kamstrim@mail.ru>
2020-09-27 21:51:11 +0300

    new

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,18 +4,18 @@ import getRandomIntInclusive from '../rnd.js';
 export default () => {
   const gameDescription = 'What number is missing in the progression?';
   const answersQuestions = []; // ['question','correctAnswer']
-  const progression = [];
-  progression.length = 10;
+  const progressions = [];
+  progressions.length = 10;
   for (let i = 0; i < roundsCount; i += 1) {
     const progressionNumberFirst = getRandomIntInclusive(1, 30);
     const progressionStep = getRandomIntInclusive(1, 30);
-    const positionDelete = getRandomIntInclusive(0, (progression.length - 1));
-    for (let n = 0; n < (progression.length - 1); n += 1) {
-      progression[n] = progressionNumberFirst + progressionStep * n;
+    const positionDelete = getRandomIntInclusive(0, (progressions.length - 1));
+    for (let n = 0; n < (progressions.length - 1); n += 1) {
+      progressions[n] = progressionNumberFirst + progressionStep * n;
     }
-    const correctAnswer = progression[positionDelete];
-    progression[positionDelete] = '..';
-    const question = String(progression);
+    const correctAnswer = progressions[positionDelete];
+    progressions[positionDelete] = '..';
+    const question = String(progressions);
     answersQuestions[i] = [question, String(correctAnswer)];
   }
   playGame(gameDescription, answersQuestions);
